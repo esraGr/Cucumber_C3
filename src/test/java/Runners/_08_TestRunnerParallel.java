59a8d5cab83b3d20e80a27f6ee4afbec119376f8
esrag <esragurevin@gmail.com>
2022-08-31 01:43:03 -0400

    renovation 2

--- src/test/java/Runners/_08_TestRunnerParallel.java
+++ src/test/java/Runners/_08_TestRunnerParallel.java
@@ -1,7 +1,5 @@
 package Runners;
 
-
-
 import Utilities.GWD;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
@@ -15,11 +13,10 @@ import org.testng.annotations.Parameters;
         features = {"src/test/java/FeatureFiles/"},
         glue = {"StepDefinitions"},
         plugin = { "html:target//cucumber-reports.html" }
-
-   )
+)
 public class _08_TestRunnerParallel extends AbstractTestNGCucumberTests {
 
-    @BeforeClass
+    @BeforeClass(alwaysRun = true) // bazı java versiyon hatalırı için
     @Parameters("browser")
     public void beforeClass(String browser)
     {
